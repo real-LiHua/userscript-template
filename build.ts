2529e8981f465263f359e5ce3e309bc9d35b15bd
Li Hua <lihua@email.com>
2026-03-26 18:09:25 +0800

    refactor: improve code style and add linting rules

--- build.ts
+++ build.ts
@@ -2,29 +2,29 @@ import { Glob } from "bun";
 import pkg from "./package.json" with { type: "json" };
 
 const tags = {
+  "@author": pkg.author ?? "you",
+  "@connect": pkg.config?.connect ?? [],
+  "@description": pkg.description,
+  "@downloadURL": pkg.config?.downloadURL,
+  "@exclude": pkg.config?.exclude ?? [],
+  "@grant": [],
+  "@homepage": pkg.homepage,
+  "@icon": pkg.config?.icon,
+  "@include": pkg.config?.include ?? [],
+  "@match": pkg.config?.match ?? [],
   "@name": pkg.name,
   "@namespace": pkg.config.namespace ?? "http://tampermonkey.net/",
-  "@version": pkg.version,
-  "@description": pkg.description,
-  "@author": pkg.author ?? "you",
-  "@homepage": pkg.homepage,
-  "@match": pkg.config?.match ?? [],
-  "@include": pkg.config?.include ?? [],
-  "@exclude": pkg.config?.exclude ?? [],
-  "@icon": pkg.config?.icon,
-  "@grant": [],
-  "@connect": pkg.config?.connect ?? [],
+  "@noframes": pkg.config?.noframes,
   "@require": pkg.config?.require ?? [],
   "@resource": pkg.config?.resource ?? [],
   "@run-at": pkg.config?.["run-at"],
   "@run-in": pkg.config?.["run-in"],
   "@sandbox": pkg.config?.sandbox,
-  "@noframes": pkg.config?.noframes,
-  "@updateURL": pkg.config?.updateURL,
-  "@downloadURL": pkg.config?.downloadURL,
   "@supportURL":
     pkg.config?.supportURL ?? pkg.repository?.url ?? pkg.repository,
   "@tag": pkg.keywords ?? [],
+  "@updateURL": pkg.config?.updateURL,
+  "@version": pkg.version,
 };
 
 const apis = new Set<string>();
@@ -32,7 +32,7 @@ const pattern =
   /\b(GM[._][^()},;\n]+|unsafeWindow|window\.(?:close|focus|onurlchange))[.,()};]/g;
 
 for (const file of await Array.fromAsync(
-  new Glob("**/*.ts").scan({ cwd: "./src", absolute: true }),
+  new Glob("**/*.ts").scan({ absolute: true, cwd: "./src" }),
 )) {
   let match;
   while ((match = pattern.exec(await Bun.file(file).text())) !== null) {
@@ -47,7 +47,9 @@ for (const api of apis) {
 const maxLabelLen = Math.max(...Object.keys(tags).map((k) => k.length));
 
 const headerLines = Object.entries(tags).flatMap(([label, value]) => {
-  if (!value) return [];
+  if (!value) {
+    return [];
+  }
   const padding = " ".repeat(maxLabelLen - label.length);
   if (Array.isArray(value)) {
     return value.map((v) => `// ${label}${padding} ${v}`);
@@ -66,10 +68,10 @@ const banner = [
 const footer = "})();";
 
 await Bun.build({
-  entrypoints: ["./src/index.ts"],
-  outdir: "./dist",
-  env: "inline",
-  minify: true,
   banner,
+  entrypoints: ["./src/index.ts"],
+  env: "inline",
   footer,
+  minify: true,
+  outdir: "./dist",
 });
